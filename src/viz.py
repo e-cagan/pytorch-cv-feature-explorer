0dec2bec2732abf4ac0efbb431e1c44c7ec2b1ac
e-cagan <emincaganapaydin@gmail.com>
2026-01-16 21:33:27 +0300

    Updated viz.py, implemented plottings for accuracy, confusion matrix and loss curves to visualize.

--- src/viz.py
+++ src/viz.py
@@ -35,26 +35,99 @@ def plot_learning_curves(filepath):
     # Plot loss curves for training and validation
     plt.figure(figsize=(10, 6))
     plt.plot(df['epoch'], df['train_loss'], label='Training Loss')
-    plt.plot(df['epoch'], df['val_loss'], label='Validation Loss')
+    plt.plot(df['epoch'], df['validation_loss'], label='Validation Loss')
     plt.xlabel('Epoch')
     plt.ylabel('Loss')
     plt.title('Learning Curves')
     plt.legend()
     plt.grid(True)
     plt.savefig('outputs/plots/loss_curves.png')
-    plt.show()
+
+    # INFO
+    print("Plot saved to outputs/plots as loss_curves.png")
 
     # Plot accuracies for training and validation
     plt.figure(figsize=(10, 6))
     plt.plot(df['epoch'], df['train_accuracy'], label='Training Accuracy')
     plt.plot(df['epoch'], df['validation_accuracy'], label='Validation Accuracy')
     plt.xlabel('Epoch')
-    plt.ylabel('Loss')
-    plt.title('Learning Curves')
+    plt.ylabel('Accuracy')
+    plt.title('Accuracy Curves')
     plt.legend()
     plt.grid(True)
     plt.savefig('outputs/plots/accuracies.png')
-    plt.show()
+
+    # INFO
+    print("Plot saved to outputs/plots as accuracies.png")
+
+    return
 
 
 # Define a function for visualizing the confusion matrix
+def plot_confusion_matrix(model, dataloader):
+    """
+    Function for visualizing confusion matrix of the predictions.
+    
+    model -> Model to visualize
+
+    dataloader -> Dataloader to consider
+    """
+
+    # Creating lists to store correct and predicted labels
+    y_true = []
+    y_pred = []
+
+    with torch.no_grad():
+        # Iterate trough batches
+        for image, label in dataloader:
+            # Move image and label to device
+            image = image.to(device)
+            label = label.to(device)
+
+            # Take the predictions
+            outputs = model(image)
+            preds = torch.argmax(outputs, dim=1)
+
+            # CPU + Flatten + tolist (Convert tensors to numpy format)
+            y_true.extend(label.detach().cpu().tolist())
+            y_pred.extend(preds.detach().cpu().tolist())
+    
+    # Define the confusion matrix
+    conf_mat = confusion_matrix(y_true, y_pred)
+
+    # Visualize confusion matrix
+    fig, ax = plt.subplots()
+    img = ax.imshow(conf_mat)
+
+    ax.set_xticks(range(10)) # 10 classes
+    ax.set_yticks(range(10)) # 10 classes
+    ax.set_xlabel('Predicted Label')
+    ax.set_ylabel('True Label')
+    ax.set_title('Confusion Matrix')
+    plt.colorbar(img, ax=ax)
+    plt.savefig('outputs/plots/confusion_matrix.png')
+
+    # INFO
+    print("Plot saved to outputs/plots as confusion_matrix.png")
+
+    return
+
+
+# Define a function for visualizing misclassified examples
+def plot_misclassified_examples(model, dataloader):
+    """
+    Function for visualizing misclassified examples of the predictions.
+    
+    model -> Model to visualize
+
+    dataloader -> Dataloader to consider
+    """
+
+    
+
+# Testing out the visualization functions
+if __name__ == '__main__':
+    
+    # For now test out the learning curves and confusion matrix
+    plot_learning_curves(filepath='outputs/logs/metrics.csv')
+    plot_confusion_matrix(model=model, dataloader=test_dataloader)
